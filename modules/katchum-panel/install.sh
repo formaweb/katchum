9f7751f409925d32e4ccfb0292d7887b1f8d4096
Hugo Leonardo Demiglio <hugo@formaweb.com.br>
2014-02-25 11:09:04 -0300

    Fixing name of panel module.

--- modules/katchum-panel/install.sh
+++ modules/katchum-panel/install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-install_apc() {
-
+install_katchum_panel() {
   curl get.mojolicio.us | sh
   curl -L cpanmin.us | perl - App::cpanminus
   cpanm Passwd::Unix
 
   mkdir /var/www/katchum
   cp ${directory}/panel /var/www/katchum
-
 }
